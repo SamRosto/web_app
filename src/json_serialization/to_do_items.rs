24e1019a26432177c4f5589cd03bbf7a3a7bcebe
xsub0x <ryaguzov.m@gmail.com>
2023-09-17 17:12:31 +0300

    deser to_do_item added

--- src/json_serialization/to_do_items.rs
+++ src/json_serialization/to_do_items.rs
@@ -1,10 +1,21 @@
-#![allow(warnings)]
 /// Define what is returned to the viewer when `get` view is called 
 /// in the current file
 
-use serde::Serialize;
+use serde::{Serialize};
+use serde_json::value::Value;
+use serde_json::Map;
+
+use actix_web:: {
+    body::BoxBody,
+    http::header::ContentType,
+    HttpRequest, HttpResponse, Responder
+};
+
 use crate::to_do::ItemTypes;
 use crate::to_do::structs::base::Base;
+use crate::state::read_file;
+use crate::to_do::{to_do_factory, enums::TaskStatus};
+
 
 #[derive(Serialize)]
 pub struct ToDoItems {
@@ -35,5 +46,28 @@ impl ToDoItems {
             pending_item_cnt: pending_cnt,
             done_item_cnt: done_cnt,
         }
+    }
+
+    pub fn get_state() -> ToDoItems {
+        let state: Map<String, Value> = read_file("./state.json");
+        let mut arr_buffer = Vec::new();
+
+        for (key, value) in state {
+            let status = TaskStatus::from_string(&value.as_str().unwrap().to_string());
+            let item = to_do_factory(&key, status);
+            arr_buffer.push(item);
+        }
+
+        ToDoItems::new(arr_buffer)
+    }
+}
+
+/// Serialize `ToDoItems` struct using the `serde_json` crate, and return HTTP response with the `ToDoItems` at the body
+impl Responder for ToDoItems {
+    type Body = BoxBody;
+
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        let body = serde_json::to_string(&self).unwrap();
+        HttpResponse::Ok().content_type(ContentType::json()).body(body)
     }
 }
--- src/views/to_do/get.rs
+++ src/views/to_do/get.rs
@@ -1,23 +1,24 @@
-use actix_web::{web, Responder};
-use serde_json::value::Value;
-use serde_json::Map;
+use actix_web::Responder;
+// use serde_json::value::Value;
+// use serde_json::Map;
 
-use crate::state::read_file;
-use crate::to_do::{ItemTypes, to_do_factory, enums::TaskStatus};
+// use crate::state::read_file;
+// use crate::to_do::{ItemTypes, to_do_factory, enums::TaskStatus};
 use crate::json_serialization::to_do_items::ToDoItems;
 
 pub async fn get() -> impl Responder {
-    let state: Map<String, Value> = read_file("./state.json");
+    // let state: Map<String, Value> = read_file("./state.json");
 
-    let mut arr_buffer = Vec::new();
+    // let mut arr_buffer = Vec::new();
 
-    for (key, val) in state {
-        let status = TaskStatus::from_string(&val.as_str().unwrap().to_string());
-        let item: ItemTypes = to_do_factory(&key, status);
-        arr_buffer.push(item);
-    }
+    // for (key, val) in state {
+    //     let status = TaskStatus::from_string(&val.as_str().unwrap().to_string());
+    //     let item: ItemTypes = to_do_factory(&key, status);
+    //     arr_buffer.push(item);
+    // }
 
-    let return_package: ToDoItems = ToDoItems::new(arr_buffer);
+    // let return_package: ToDoItems = ToDoItems::new(arr_buffer);
 
-    web::Json(return_package)
+    // web::Json(return_package)
+    ToDoItems::get_state()
 }
